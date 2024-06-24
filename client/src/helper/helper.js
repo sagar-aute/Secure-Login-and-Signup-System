49f3f56c2efc2b14d0652a4b908e907a61cb308e
sagar-aute <sagaraute090@gmail.com>
2024-06-25 02:12:05 +0530

    changes

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -18,7 +18,7 @@ export async function getUsername(){
 /** authenticate function */
 export async function authenticate(username){
     try {
-        return await axios.post('/api/authenticate', { username })
+        return await axios.post('https://secure-login-and-signup-system.vercel.app/api/authenticate', { username })
     } catch (error) {
         return { error : "Username doesn't exist...!"}
     }
@@ -37,13 +37,13 @@ export async function getUser({ username }){
 /** register user function */
 export async function registerUser(credentials){
     try {
-        const { data : { msg }, status } = await axios.post(`/api/register`, credentials);
+        const { data : { msg }, status } = await axios.post(`https://secure-login-and-signup-system.vercel.app/api/register`, credentials);
 
         let { username, email } = credentials;
 
         /** send email */
         if(status === 201){
-            await axios.post('/api/registerMail', { username, userEmail : email, text : msg})
+            await axios.post('https://secure-login-and-signup-system.vercel.app/api/registerMail', { username, userEmail : email, text : msg})
         }
 
         return Promise.resolve(msg)
@@ -56,7 +56,7 @@ export async function registerUser(credentials){
 export async function verifyPassword({ username, password }){
     try {
         if(username){
-            const { data } = await axios.post('/api/login', { username, password })
+            const { data } = await axios.post('https://secure-login-and-signup-system.vercel.app/api/login', { username, password })
             return Promise.resolve({ data });
         }
     } catch (error) {
@@ -86,7 +86,7 @@ export async function generateOTP(username){
         if(status === 201){
             let { data : { email }} = await getUser({ username });
             let text = `Your Password Recovery OTP is ${code}. Verify and recover your password.`;
-            await axios.post('/api/registerMail', { username, userEmail: email, text, subject : "Password Recovery OTP"})
+            await axios.post('https://secure-login-and-signup-system.vercel.app/api/registerMail', { username, userEmail: email, text, subject : "Password Recovery OTP"})
         }
         return Promise.resolve(code);
     } catch (error) {
